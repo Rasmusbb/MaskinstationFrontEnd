4cd507b43f1c224c2805f87de1a2ea1fa45ee0ee
Rasmusbb <Rasmus@vhma.dk>
2026-03-08 17:53:55 +0100

    fix

--- src/lib/Compontnets/NavBars/VerticalNavBar.svelte
+++ src/lib/Compontnets/NavBars/VerticalNavBar.svelte
@@ -4,7 +4,7 @@
   const items = [
     { id: '', label: 'Hjem', icon: '🏠' },
     { id: '/Stock', label: 'Vare', icon: '📦' },
-    { id: '/Costumers', label: 'Kunder', icon: '👤' }
+    { id: '/Costumers', label: 'Kunder', icon: '👤' },
     { id: '/Employees', label: 'Medarbejdere', icon: '👨‍🌾'},
     { id: '/Sitesettings', label: 'SiteSettings', icon: '⚙️' },
   ];
